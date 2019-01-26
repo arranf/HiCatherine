ac59d27c5a6b703f8dc014b2b6ea440f402cadc8
Catherine France <catherinefoster95@gmail.com>
2019-01-26 22:43:11 +0000

    Rework

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,18 @@
   $(document).ready(function(){
     $('.scrollspy').scrollSpy();
     $('.pushpin').pushpin({
-      top: $('.pushpin').offset().top
+      top: $('.pushpin').offset().top,
+      onPositionChange: function(position){
+        var main = $('.main');
+        if(position == "pinned"){
+          main.addClass("offset-m4");
+          main.addClass("offset-l4");
+        }
+        else{
+          main.removeClass("offset-m4");
+          main.removeClass("offset-l4");
+        }
+      }
     });
   });
         
